c1bda197e668dc2a48664054d1d651f6669abcae
scriveausti <scrive.austi@awatapu.school.nz>
2022-08-01 09:56:39 +1200

    e

--- basic_functions/number_checker.py
+++ basic_functions/number_checker.py
@@ -1,1 +1,14 @@
-def number_chec
+def number_checker(question):
+    while True:
+        try:
+            number = int(input(question))
+            return number
+        except:
+            print("<error> please enter a number")
+            print("")
+
+
+
+if __name__ == '__main__':
+    num = number_checker("enter a number ")
+    print(num)
--- box/box_dimetions.py
+++ box/box_dimetions.py
@@ -1,7 +1,13 @@
-from customers_name_ask import customers_ask_1
-
+from customers_name_ask.customers_ask_1 import ask_name
+from basic_functions.number_checker import number_checker
 
 
 def box_dim(user_name):
-    box_hight = input("Hello {}, how tall is the box you will be returning your item/s in (cm)".format(user_name))
-    box_with = input()
+    box_height = number_checker("Hello {}, how tall is the box you will be returning your item/s? (cm) ".format(user_name))
+    box_width = number_checker("what is the width of your box? ")
+    box_depth = number_checker("what is the depth of your box? ")
+    box_volume = box_depth * box_height * box_width
+
+    return box_volume
+if __name__ == '__main__':
+    name = ask_name()
