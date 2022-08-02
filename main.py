5de8c9629f48a1938e764f70dc03012f69cb7175
austin <scrive.austi@awatapu.school.nz>
2022-08-03 10:43:56 +1200

    e

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
     welcome()
     user_name = ask_name()
     total_price = box_dim(user_name)
-    total_price =ask_island(total_price)
+    total_price = ask_island(total_price)
     user_info = ask_other_info(user_name,total_price)
-    print("users name : {} {}, user's address : {}, user's phone number : {}, total returning price : ${}".format(user_info[0],user_info[1],user_info[2],user_info[3],user_info[4]))
+    print("user's name : {} {}, user's address : {}, user's phone number : {}, total returning price : ${}".format(user_info[0],user_info[1],user_info[2],user_info[3],user_info[4]))
